cdb5c84a09c678cb78114af27447640d314506e2
albornoz <albornoz_13-25@hotmail.com>
2018-04-19 11:13:35 +0200

    Nueva Actizacion de Javascript

---
+++ HTML/javascript/js/Estilo.js
@@ -0,0 +1,9 @@
+alert("JavaScript Off-line");
+function Escribelo()
+{
+alert("Estoy en HOLA");
+}
+function Escribelo1()
+{
+alert("Estoy en Adios");
+}
---
+++ HTML/javascript/js/Estilo2.js
@@ -0,0 +1,5 @@
+alert("JavaScript Off-line en el fichero2");
+function Escribelo2()
+{
+alert("Estoy en HOLA");
+}
